3e7880d157f7b0ebac49505c09e7ce5579e9c668
CypressXt <cypress@cypressxt.net>
2014-04-10 19:06:32 +0200

    Adding some files
    Header/footer + update css

---
+++ view/Footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <h6>CypressXt - Naytskai<br><small>2014 - ∞ ©</small></h6>
+</footer>
+</div>
+</body>
+</html>
---
+++ view/Header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/style/Global.css">
+        <title>DuoQ</title>
+    </head>
+    <body>
+        <?php include_once 'Menu.php'; ?>
+        <div id="wrapper">
+        
