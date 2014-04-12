b71e7c201a2cd731a3998b1fd2a123e79f9b7f93
Naytskai <naytskaid@gmail.com>
2014-04-12 21:10:45 +0200

    Code Cleaning

--- index.php
+++ index.php
@@ -1,122 +1,18 @@
 <?php
+    $l = isset($_GET['l']) ? $_GET['l'] : false;
 
-<<<<<<< HEAD
-    require_once 'sqlConnect.php';
-    require_once 'model/api/LolApi.php';
-    require_once 'model/api/DisplayQuery.php';
-
+    switch ($l) {
+        case false:
+            include_once('controller/cIndex.php');
+            break;
+        case "login":
+            include_once('controller/cLogin.php');
+            break;
+        case "download";
+            include_once ('controller/cDownload.php');
+            break;
+        case "about";
+            include_once ('controller/cAbout.php');
+            break;
+    }
 ?>
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link href="style.css" rel="stylesheet" type="text/css">
-        <title></title>
-    </head>
-    <body>
-        
-        <?php
-        
-            LolApi::init($db,$keys);
-            
-            echo "<pre>";
-            
-//            var_dump(LolApi::getSummonerById(19447356));
-            
-//            var_dump(LolApi::getSummonerIdByName("Naytskaï"));
-            
-//                LolApi::getDuoRankedGames("Gardok4","Naytskaï");
-            
-                LolApi::getChampions();
-
-//            echo LolApi::getDuoRankedGames("Gardok4","Naytskaï");
-            
-//            var_dump(LolApi::getCurrentPatch());
-            
-//                var_dump(LolApi::getLeagueInfo(19447356));
-                
-//            var_dump(LolApi::getRecentRankedGameBySummonerIdAndMatch(19447356,1405318508));
-            
-            
-            
-//            LolApi::getRecentRankedGamesBySummonerId(19447356);
-               
-//            $champions = LolApi::getChampions();
-//            
-//            echo $champions[43];
-            
-            echo "</pre>";
-            
-//            foreach ($array as $games)
-//            {
-//                $t = 0;
-//                echo "<table>";
-//                
-//                echo "<tr>";
-//                    echo "<th>Summoner</th>";
-//                    echo "<th>Champion</th>";
-//                    echo "<th>level</th>";
-//                    echo "<th>Kill</th>";
-//                    echo "<th>Death</th>";
-//                    echo "<th>Assist</th>";
-//                    echo "<th>cs</th>";
-//                    echo "<th>gold</th>";
-//                    echo "<th>damage</th>";
-//                echo "</tr>";
-//                
-//                foreach ($games as $game)
-//                {
-//                
-//                    foreach ($game as $keys => $team)
-//                    {
-//
-//                        foreach ($team as $player)
-//                        {
-//                            echo "<tr>";
-//                 
-//                            foreach ($player as $keys => $stats)
-//                            {
-//                                echo "<td>$stats</td>";
-//                            }
-//                            
-//                            echo "</tr>"; 
-//                            $t++;
-//
-//                        }
-//                    }
-//                }
-//                
-//                echo "</table>";
-//            }
-//   
-//            echo "</pre>";
-            
-        ?>
-        
-        
-    </body>
-</html>
-=======
-
-$l = isset($_GET['l']) ? $_GET['l'] : false;
-
-switch ($l) {
-    case false:
-        include_once('controller/cIndex.php');
-        break;
-    case "login":
-        include_once('controller/cLogin.php');
-        break;
-    case "download";
-        include_once ('controller/cDownload.php');
-        break;
-    case "about";
-        include_once ('controller/cAbout.php');
-        break;
-}
->>>>>>> b2db82afd094cb7032e9b2ae761d7210048c4e7e
