f552b2e1fa38d7b78ec73ce83ba283537516985b
CypressXt <cypress@cypressxt.net>
2014-04-12 17:49:33 +0200

    Create "base view" and update Global Css

--- view/Footer.php
+++ view/Footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <h6>CypressXt - Naytskai<br><small>2014 - ∞ ©</small></h6>
+    <h6>CypressXt - Naytskai<br><small>2014 - ∞ ©</small><br></h6>
 </footer>
 </div>
 </body>
---
+++ view/Menu.php
@@ -0,0 +1,24 @@
+<header>
+    <a href="">
+        <button type="button" class="btn btn-default">
+            Home
+        </button>
+    </a>
+
+    <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+            Lanes
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="">Browse statistics</a></li>
+            <li><a href="">New duo lane</a></li>
+        </ul>
+    </div>
+
+    <a href="">
+        <button type="button" class="btn btn-default">
+            Account
+        </button>
+    </a>
+</header>
