87bf692bb340159c6ee41b5410e0897004ecf039
CypressXt <cypress@cypressxt.net>
2014-04-17 14:30:01 +0200

    update login & inscription formular

--- controller/cLogin.php
+++ controller/cLogin.php
@@ -1,9 +1,33 @@
 <?php
 
-if($_SESSION){
+checkLogin();
+checkRegister();
 
-}else {
+
+
+if ($_SESSION) {
+    
+} else {
     include_once 'view/Header.php';
     include_once 'view/vLogin.php';
     include_once 'view/Footer.php';
+}
+
+function checkRegister() {
+    if (isset($_POST['submitRegister'])) {
+        $userName = $_POST['newUserName'];
+        $newMail = $_POST['newMail'];
+        $newPass = $_POST['newPassword'];
+        $newPassConf = $_POST['newPasswordConf'];
+    }
+}
+
+function checkLogin() {
+    if (isset($_POST['submitLogin'])) {
+        $userEmai = $_POST['mail'];
+        $userPassword = $_POST['password'];
+        
+        
+        
+    }
 }
--- view/vLogin.php
+++ view/vLogin.php
@@ -1,19 +1,66 @@
-<form class="form-horizontal" role="form">
-    <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-        <div class="col-sm-4">
-            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-        </div>
+<div class="row">
+    <div class="col-md-6">
+        <form class="form-horizontal" method="Post" role="form">
+            <div class="form-group">
+                <div class="col-sm-offset-1">
+                    <h2>Login</h2>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="mail" class="col-sm-4 control-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" name="mai" id="mail" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="password" class="col-sm-4 control-label">Password</label>
+                <div class="col-sm-6">
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-6">
+                    <button type="submit" name="submitLogin" id="submitLogin" class="btn btn-default">Login</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-        <div class="col-sm-4">
-            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-        </div>
+    <div class="col-md-6 headerDivider">
+        <form class="form-horizontal" method="Post" role="form">
+            <div class="form-group">
+                <div class="col-sm-offset-1">
+                    <h2>Register</h2>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="newUsername" class="col-sm-4 control-label">Username</label>
+                <div class="col-sm-6">
+                    <input type="email" name="newUserName" class="form-control" id="newUsername" placeholder="Username">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="newMail" class="col-sm-4 control-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" name="newMail" id="newMail" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="newPassword" class="col-sm-4 control-label">Password</label>
+                <div class="col-sm-6">
+                    <input type="password" class="form-control" name="newPassword" id="newPassword" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="newPasswordConf" class="col-sm-4 control-label">Confirmation</label>
+                <div class="col-sm-6">
+                    <input type="password" class="form-control" name="newPasswordConf" id="newPasswordConf" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-6">
+                    <button type="submit" name="submitRegister" id="submitRegister" class="btn btn-default">Register</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Log in</button>
-        </div>
-    </div>
-</form>
+</div>
