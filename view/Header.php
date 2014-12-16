30b9a8293fc7c7189af4521a5c424cd682c5fd17
CypressXt <clement@CypressXtMacbookProR.local>
2014-12-16 20:40:16 +0100

    Preparing mobile optimisations

--- view/Header.php
+++ view/Header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
         <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="assets/style/bootstrap-theme.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
--- view/vIndexLogged.php
+++ view/vIndexLogged.php
@@ -1,7 +1,7 @@
 <div class="row centeredContent">
     <div class="col-md-3">
         <div class="thumbnail">
-            <img width="50%" height="50%" src="https://lh4.googleusercontent.com/-ZbE4firdO2k/VJBC3ep9eDI/AAAAAAAACM0/gDT1__OMPH4/s1000-no/meetNPlayers2.png">
+            <img height="120px" src="https://lh4.googleusercontent.com/-ZbE4firdO2k/VJBC3ep9eDI/AAAAAAAACM0/gDT1__OMPH4/s1000-no/meetNPlayers2.png">
             <div class="caption">
                 <h3>Meet new players</h3>
                 <p>Need an other player to start ranked game ?</p>
@@ -11,7 +11,7 @@
     </div> 
     <div class="col-md-3">
         <div class="thumbnail">
-            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-qSuDwUDS2t4/VJBCMw1Mj-I/AAAAAAAACMk/C0Oqro1_wos/s1000-no/duo2.png">
+            <img height="120px" src="https://lh5.googleusercontent.com/-qSuDwUDS2t4/VJBCMw1Mj-I/AAAAAAAACMk/C0Oqro1_wos/s1000-no/duo2.png">
             <div class="caption">
                 <h3>Duo lane</h3>
                 <p>Register here your new duo queue, or display them all.</p>
@@ -22,7 +22,7 @@
     </div>
     <div class="col-md-3">
         <div class="thumbnail">
-            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-EwD0ECBKJtQ/VJA9nxbk4gI/AAAAAAAACLs/yJb0nNOf1Co/s1000-no/advancedStats.png">
+            <img height="120px" src="https://lh5.googleusercontent.com/-EwD0ECBKJtQ/VJA9nxbk4gI/AAAAAAAACLs/yJb0nNOf1Co/s1000-no/advancedStats.png">
             <div class="caption">
                 <h3>Statistics</h3>
                 <p>Display your matches results and statistics by duo.</p>
@@ -32,7 +32,7 @@
     </div>
     <div class="col-md-3">
         <div class="thumbnail">
-            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-yvi3bUk5gEE/VJA_uu-L1uI/AAAAAAAACL8/TERb4lozbZY/s1000-no/progression.png">
+            <img height="120px" src="https://lh5.googleusercontent.com/-yvi3bUk5gEE/VJA_uu-L1uI/AAAAAAAACL8/TERb4lozbZY/s1000-no/progression.png">
             <div class="caption">
                 <h3>Progression</h3>
                 <p>Show you the progression based on your tracked duo.</p>
