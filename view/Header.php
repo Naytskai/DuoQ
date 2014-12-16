0c15d33b863ff4ae5c83e1b290edddf3ccd9d670
CypressXt <clement@CypressXtMacbookProR.local>
2014-12-16 15:55:24 +0100

    Look & feel updated

--- view/Header.php
+++ view/Header.php
@@ -13,6 +13,23 @@
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.3.5/bootstrap-select.min.js"></script>
         <script type="text/javascript" src="assets/javascript/helper.js"></script>
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-57720049-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
         <title><?php echo $pageName ?></title>
     </head>
     <body>
--- view/vIndexDefault.php
+++ view/vIndexDefault.php
@@ -1,29 +1,41 @@
-<div class="row centeredContent">
-    <div class="col-md-4">
+<div class="row centeredContent homePageRow">
+    <div class="col-md-3">
         <div class="thumbnail">
-            <img height="120" width="120" src="https://lh5.googleusercontent.com/-M4aLbyJaY5g/U0mLGnlmd1I/AAAAAAAAB6M/VbaJ6l5QR5A/s1000-no/user_icon.png">
+            <img width="50%" height="50%" src="https://lh4.googleusercontent.com/-ZbE4firdO2k/VJBC3ep9eDI/AAAAAAAACM0/gDT1__OMPH4/s1000-no/meetNPlayers2.png">
             <div class="caption">
-                <h3>Create custom duo lane</h3>
-                <p>...</p>
+                <h3>Meet new players</h3>
+                <p>Get in touch with other ranked players, sorted by lane preferences, by league, or by language.</p>
+            </div>
+        </div>
+    </div> 
+    <div class="col-md-3">
+        <div class="thumbnail">
+            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-qSuDwUDS2t4/VJBCMw1Mj-I/AAAAAAAACMk/C0Oqro1_wos/s1000-no/duo2.png">
+            <div class="caption">
+                <h3>Create duo lane</h3>
+                <p>Once you have found a mate, simply type his summoner's name and your duo ranked queue is now tracked !</p>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-3">
         <div class="thumbnail">
-            <img height="120" width="120" src="https://lh3.googleusercontent.com/-yGhanEX3_2M/U0li3x7Nu9I/AAAAAAAAB5Y/ZQwOQeOeePM/s256-no/graphique-tarte-icone-7508-128.png">
+            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-EwD0ECBKJtQ/VJA9nxbk4gI/AAAAAAAACLs/yJb0nNOf1Co/s1000-no/advancedStats.png">
             <div class="caption">
-                <h3>Get advanced statistics</h3>
-                <p>...</p>
+                <h3>Advanced statistics</h3>
+                <p>All your ranked queues are tracked and provide you some advanced statistics, 
+                    like the average gold earned by game, the average death time by game, the average damage dealt by game and lot more.
+                </p>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-3">
         <div class="thumbnail">
-            <img height="120" width="120" src="https://lh5.googleusercontent.com/-sj5goJp0wPk/U0mG1p3wYDI/AAAAAAAAB6A/9H7CpJaa-rI/s384-no/Challenger_i.png">
+            <img width="50%" height="50%" src="https://lh5.googleusercontent.com/-yvi3bUk5gEE/VJA_uu-L1uI/AAAAAAAACL8/TERb4lozbZY/s1000-no/progression.png">
             <div class="caption">
-                <h3>Manage your progression</h3>
-                <p>...</p>
+                <h3>Progress</h3>
+                <p>You can now have access to a lot of precise data giving you 
+                    the keys to improve your skill with personalised tips.</p>
             </div>
         </div>
-    </div>    
+    </div>
 </div>
